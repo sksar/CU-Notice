b636d3551ac9f398934ac8c02f485631e40c3f6d
Sweety Ahmed <sweety127ahmed@gmail.com>
2020-12-16 22:47:51 +0530

    Fetch & convert to array

--- build.js
+++ build.js
@@ -1,7 +1,28 @@
-// TODO: GET contents of URL
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
 
+const NOTICES = [];
 
-// TODO: Convert information to Array 
+async function MAIN() {
+    
+    // FETCH FROM URL
+    let result = await fetch('https://cuexam.net/exam-notice.php');
+
+    // LOAD IN PARSER (MINI-WEB_BROWSER)
+    const $ = cheerio.load(await result.text());
+
+    // GET
+    $('table.embeddedTable table tr').each((index, element) => {
+        let TITLE = $(element).find('a').text();
+        let URL = $(element).find('a').attr('href');
+        let DATE = $(element).find('td:nth-child(3) span').text();
+
+        // TODO: Modify to get latest date wise, not first 10
+        if (index > 10) return false;
+        if (index) NOTICES.push({ TITLE, URL, DATE });
+    });
+
+}
 
 
 // TODO: Fetch Files & Generate HTML
