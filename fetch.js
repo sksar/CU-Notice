7fe5c5f6c2401f03c39c8ff2e58ed3309e94431e
Sweety Ahmed <sweety127ahmed@gmail.com>
2020-12-17 18:40:22 +0530

    Download PDFs

--- fetch.js
+++ fetch.js
@@ -1,18 +1,23 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
+const fs = require('fs');
+const download = require('download');
+
 
 const NOTICES = [];
 
 async function MAIN() {
-    
+
     // FETCH FROM URL
     let result = await fetch('https://cuexam.net/exam-notice.php');
 
-    // LOAD IN PARSER (MINI-WEB_BROWSER)
+    // LOAD IN PARSER
     const $ = cheerio.load(await result.text());
 
     // GET
     $('table.embeddedTable table tr').each((index, element) => {
+
+        // Extract information about each notice from tr (element)
         let TITLE = $(element).find('a').text();
         let URL = $(element).find('a').attr('href');
         let DATE = $(element).find('td:nth-child(3) span').text();
@@ -20,10 +25,15 @@ async function MAIN() {
         // TODO: Modify to get latest date wise, not first 10
         if (index > 10) return false;
         if (index) NOTICES.push({ TITLE, URL, DATE });
+
+    });
+
+    NOTICES.map(async (notice) => {
+        await download(`https://cuexam.net/` + notice.URL, __dirname + '/notices');
+        console.log('Downloaded ', notice.TITLE);
     });
 
 }
+MAIN();
 
-
-// TODO: Fetch Files & Generate HTML
 
